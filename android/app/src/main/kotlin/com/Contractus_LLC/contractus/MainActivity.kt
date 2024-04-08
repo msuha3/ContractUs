d67b4f00dcf8473825e0c04da61620f9482ae7ab
Rizwan Ahmed <msuha3@uic.edu>
2024-04-08 08:50:21 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/Contractus_LLC/contractus/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.Contractus_LLC.contractus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:contractus/screen/splash%20screen/mt_splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contract Us',
+      theme: ThemeData(fontFamily: 'Display'),
+      home: const SplashScreen(),
+    );
+  }
+}
